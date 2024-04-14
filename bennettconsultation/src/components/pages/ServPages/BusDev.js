83f9d8df87a124057d5246a7b5348ec90a2c1cba
AJoelj84 <ajoelj84@gmail.com>
2024-04-14 13:14:40 -0400

    More Changes to Business Development Styling

--- bennettconsultation/src/components/pages/ServPages/BusDev.js
+++ bennettconsultation/src/components/pages/ServPages/BusDev.js
@@ -8,7 +8,7 @@ function BusDev(){
             <Services/>
             <section className='card-containecard-container row row-cols-sm-2 g-3'>
             <p className='card'>
-                <h1 className='card'>Market Research and Analysis</h1>
+                <h1 className='busheader'>Market Research and Analysis</h1>
                 Conduct comprehensive market research to identify 
                 potential growth opportunities, 
                 target markets, and emerging trends. 
@@ -17,31 +17,31 @@ function BusDev(){
                 strategic decision-making.
             </p>
                 
-            <p className='col'>
-                <h1 className='card'>Strategic Planning</h1> <br></br>
+            <p className='card'>
+                <h1 className='busheader'>Strategic Planning</h1> <br></br>
                 Collaborate with clients to develop tailored business development strategies 
                 <br></br>aligned with their goals and objectives. <br></br>
                 Create actionable plans encompassing market penetration, <br></br>product/service expansion, 
                 partnership development, and revenue growth strategies.
             </p>
                 
-            <p className='col'>
-                <h1 className='card'>Sales Training and Enablement</h1> <br></br>
+            <p className='card'>
+                <h1 className='busheader'>Sales Training and Enablement</h1> <br></br>
                 Provide customized sales training programs and resources to equip teams with the skills,<br></br> 
                 techniques, and tools necessary to drive business development efforts. Offer sales enablement <br></br>
                 solutions including sales collateral development, pitch refinement, and objection handling strategies.
             </p>
 
-            <p className='col'>
-                <h1 className='card'>Client Relationship Management</h1><br></br> 
+            <p className='card'>
+                <h1 className='busheader'>Client Relationship Management</h1><br></br> 
                 Implement effective client relationship management (CRM) systems and practices 
                 <br></br>to enhance customer acquisition, retention, and satisfaction.<br></br> 
                 Develop personalized approaches for client engagement, communication, 
                 <br></br>and feedback to foster long-term partnerships and loyalty.
             </p>
 
-            <p className='col'>
-                <h1 className='card'>Networking and Partnership Facilitation</h1><br></br> 
+            <p className='card'>
+                <h1 className='busheader'>Networking and Partnership Facilitation</h1><br></br> 
                 Facilitate networking opportunities and strategic partnerships 
                 <br></br>to broaden the client's reach and enhance their market presence.<br></br>
                 Identify potential collaborators, investors, and stakeholders, and 
