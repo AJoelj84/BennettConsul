24d3dfda8aa250c27351f84cb8c5fa4d9846e586
AJoelj84 <ajoelj84@gmail.com>
2024-04-14 13:08:41 -0400

    Making adjustments to Business Development page and Styling

--- bennettconsultation/src/components/pages/ServPages/BusDev.js
+++ bennettconsultation/src/components/pages/ServPages/BusDev.js
@@ -4,46 +4,51 @@ function BusDev(){
     return(
 
         
-        <div>
+        <div className='sedan-regular'>
             <Services/>
-            <p className='busdev'>
-                <h1 className='busheader'>Market Research and Analysis</h1>
-                <br></br>Conduct comprehensive market research to identify 
+            <section className='card-containecard-container row row-cols-sm-2 g-3'>
+            <p className='card'>
+                <h1 className='card'>Market Research and Analysis</h1>
+                Conduct comprehensive market research to identify 
                 potential growth opportunities, 
-                <br></br>target markets, and emerging trends. 
+                target markets, and emerging trends. 
                 Analyze industry data and competitor landscapes 
-                <br></br>to provide valuable insights for 
+                to provide valuable insights for 
                 strategic decision-making.
             </p>
                 
-            <p className='busdevright'>
-                <h1 className='busheaderright'>Strategic Planning</h1> <br></br>
+            <p className='col'>
+                <h1 className='card'>Strategic Planning</h1> <br></br>
                 Collaborate with clients to develop tailored business development strategies 
                 <br></br>aligned with their goals and objectives. <br></br>
                 Create actionable plans encompassing market penetration, <br></br>product/service expansion, 
                 partnership development, and revenue growth strategies.
             </p>
                 
-            <p className='busdev'>
-                <h1 className='busheader'>Sales Training and Enablement</h1> <br></br>
+            <p className='col'>
+                <h1 className='card'>Sales Training and Enablement</h1> <br></br>
                 Provide customized sales training programs and resources to equip teams with the skills,<br></br> 
                 techniques, and tools necessary to drive business development efforts. Offer sales enablement <br></br>
                 solutions including sales collateral development, pitch refinement, and objection handling strategies.
             </p>
 
-            <p className='busdevright'>
-                <h1 className='busheaderright'>Client Relationship Management</h1><br></br> 
-                Implement effective client relationship management (CRM) systems and practices to enhance customer acquisition, retention, and satisfaction.<br></br> 
-                Develop personalized approaches for client engagement, communication, and feedback to foster long-term partnerships and loyalty.
+            <p className='col'>
+                <h1 className='card'>Client Relationship Management</h1><br></br> 
+                Implement effective client relationship management (CRM) systems and practices 
+                <br></br>to enhance customer acquisition, retention, and satisfaction.<br></br> 
+                Develop personalized approaches for client engagement, communication, 
+                <br></br>and feedback to foster long-term partnerships and loyalty.
             </p>
 
-            <p className='busdev'>
-                <h1 className='busheader'>Networking and Partnership Facilitation</h1><br></br> 
-                Facilitate networking opportunities and strategic partnerships to broaden the client's reach and enhance their market presence.<br></br>
-                Identify potential collaborators, investors, and stakeholders, and facilitate negotiations and agreements to forge mutually beneficial alliances.
+            <p className='col'>
+                <h1 className='card'>Networking and Partnership Facilitation</h1><br></br> 
+                Facilitate networking opportunities and strategic partnerships 
+                <br></br>to broaden the client's reach and enhance their market presence.<br></br>
+                Identify potential collaborators, investors, and stakeholders, and 
+                <br></br>facilitate negotiations and agreements to forge mutually beneficial alliances.
             </p>
 
-
+            </section>
        </div>
     );
 }
