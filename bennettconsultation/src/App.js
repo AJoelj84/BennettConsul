8241b16b6e02251df52aa05a5096377089f0c918
AJoelj84 <ajoelj84@gmail.com>
2024-04-15 19:02:26 -0400

    Added Commenting and an additional <section> for Page background image

--- bennettconsultation/src/App.js
+++ bennettconsultation/src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-
-
+// All Main Page and Subsection of Pages Import
 import Navigation from './components/Navigation';
 import Home from './components/pages/Home';
 import Biography from './components/pages/Biography';
@@ -16,12 +15,14 @@ import LifeScSaaS from './components/pages/ServPages/LifeScSaaS';
 import ToxSD from './components/pages/ServPages/ToxSD';
 import Footer from './components/Footer';
 
-
 function App() {
   return (
     <Router>
-      <div>
+      <div> 
         <h1 className='sedan-regular topbar'>Dr. Allen Bennett Ph.D.<br></br>Consultation Services<Navigation/></h1>
+
+        <section>
+
         <Routes>
           <Route path = "/" element={<Home/>} exact />
           <Route path = "/Home" element = {<Home/>}/>
@@ -29,13 +30,19 @@ function App() {
           <Route path = "/Contact" element={<Contact/>} />
           <Route path = "/Publications" element={<Publications/>} />
           <Route path = "/Services" element={<Services/>} />
+
+          {/*All Subpages*/}
           <Route path = "/BusDev" element ={<BusDev/>}/>
           <Route path = "/ContROIS" element ={<ContROIS/>}/>
           <Route path = "/ExpDesInt" element ={<ExpDesInt/>}/>
           <Route path = "/LifeScSaaS" element ={<LifeScSaaS/>}/>
           <Route path = "/ToxSD" element ={<ToxSD/>}/>
         </Routes>
+
         <Footer/>
+
+        </section>
+
       </div>
     </Router>
   );
