a2af00c0adb822c4381234029b3a8d06df72dde6
AJoelj84 <ajoelj84@gmail.com>
2024-04-15 23:27:13 -0400

    Added Toxicology Study Design Page text and set styling

--- bennettconsultation/src/components/pages/ServPages/BusDev.js
+++ bennettconsultation/src/components/pages/ServPages/BusDev.js
@@ -21,34 +21,34 @@ function BusDev(){
             </p>
                 
             <p className='busright'>
-                <h1 className='busheader'>Strategic Planning</h1> <br></br>
+                <h1 className='busheader'>Strategic Planning</h1>
                 Collaborate with clients to develop tailored business development strategies 
-                <br></br>aligned with their goals and objectives. <br></br>
-                Create actionable plans encompassing market penetration, <br></br>product/service expansion, 
+                aligned with their goals and objectives. 
+                Create actionable plans encompassing market penetration, product/service expansion, 
                 partnership development, and revenue growth strategies.
             </p>
                 
             <p className='busleft'>
-                <h1 className='busheader'>Sales Training and Enablement</h1> <br></br>
-                Provide customized sales training programs and resources to equip teams with the skills,<br></br> 
-                techniques, and tools necessary to drive business development efforts. Offer sales enablement <br></br>
+                <h1 className='busheader'>Sales Training and Enablement</h1>
+                Provide customized sales training programs and resources to equip teams with the skills, 
+                techniques, and tools necessary to drive business development efforts. Offer sales enablement 
                 solutions including sales collateral development, pitch refinement, and objection handling strategies.
             </p>
 
             <p className='busright'>
-                <h1 className='busheader'>Client Relationship Management</h1><br></br> 
+                <h1 className='busheader'>Client Relationship Management</h1>
                 Implement effective client relationship management (CRM) systems and practices 
-                <br></br>to enhance customer acquisition, retention, and satisfaction.<br></br> 
+                to enhance customer acquisition, retention, and satisfaction.
                 Develop personalized approaches for client engagement, communication, 
-                <br></br>and feedback to foster long-term partnerships and loyalty.
+                and feedback to foster long-term partnerships and loyalty.
             </p>
 
             <p className='busleft'>
-                <h1 className='busheader'>Networking and Partnership Facilitation</h1><br></br> 
+                <h1 className='busheader'>Networking and Partnership Facilitation</h1>
                 Facilitate networking opportunities and strategic partnerships 
-                <br></br>to broaden the client's reach and enhance their market presence.<br></br>
+                to broaden the client's reach and enhance their market presence.
                 Identify potential collaborators, investors, and stakeholders, and 
-                <br></br>facilitate negotiations and agreements to forge mutually beneficial alliances.
+                facilitate negotiations and agreements to forge mutually beneficial alliances.
             </p>
 
             </section>
--- bennettconsultation/src/components/pages/ServPages/ToxSD.js
+++ bennettconsultation/src/components/pages/ServPages/ToxSD.js
@@ -4,10 +4,50 @@ function ToxSD(){
     return(
 
         
-        <div>
+        <div className='sedan-regular'>
             <Services/>
-          Toxicology Page
-        </div>
+            <section className=' row row-cols-sm-2 g-3'>
+            <p className='busleft'>
+                <h1 className='busheader'>Protocol Development</h1>
+                Design comprehensive toxicology study protocols tailored to specific client 
+                needs and regulatory requirements. 
+                Incorporate best practices and industry standards to ensure the study's scientific validity,
+                ethical considerations, and compliance with regulatory guidelines.
+            </p>
+                
+            <p className='busright'>
+                <h1 className='busheader'>Dose Selection and Administration</h1>
+                Advise on appropriate dose levels and administration routes for test substances 
+                based on toxicity profiles, pharmacokinetic data, and study objectives. 
+                Optimize dosing regimens to achieve meaningful results while minimizing potential 
+                risks to study subjects.
+            </p>
+                
+            <p className='busleft'>
+                <h1 className='busheader'>Study Monitoring and Quality Assurance</h1>
+                Implement rigorous monitoring and quality assurance measures throughout the study 
+                lifecycle to maintain data integrity and regulatory compliance. Conduct ongoing oversight, 
+                data review, and audits to identify and address any deviations or issues promptly.
+            </p>
+
+            <p className='busright'>
+                <h1 className='busheader'>Data Analysis and Interpretation</h1>
+                Utilize advanced statistical methods and toxicological expertise to 
+                analyze study data and interpret findings accurately. 
+                Generate comprehensive reports summarizing study results, significance, 
+                and implications for product development, risk assessment, or regulatory submission.
+            </p>
+
+            <p className='busleft'>
+                <h1 className='busheader'>Regulatory Support and Compliance</h1>
+                Provide guidance and support for navigating regulatory requirements and 
+                preparing submissions for regulatory agencies (e.g., FDA, EMA). Ensure adherence to relevant 
+                regulations, guidelines, and standards (e.g., ICH, OECD) to facilitate successful study 
+                outcomes and regulatory approval processes.
+            </p>
+
+            </section>
+       </div>
     );
 }
 
